869f66f99bee757e0f2e089aec589e669332ff9d
HOLLYwyh <1689394668@qq.com>
2022-06-02 01:51:00 +0800

    完成收藏夹部分

---
+++ JiaYaoBackEnd/Controllers/FavoriteController.cs
@@ -0,0 +1,37 @@
+﻿using JiaYao.Models;
+using JiaYao.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JiaYao.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoriteController : Controller
+    {
+        private JiaYaoContext _context;
+
+        public FavoriteController(JiaYaoContext context)
+        {
+            _context = context;
+        }
+
+        // 获取用户收藏的食材
+        [Route("getFavoriteIngredient")]
+        [HttpGet]
+        public async Task<ActionResult<List<Ingredient>>> getFavoriteIngredient([FromHeader] string myAuthentication)
+        {
+            return await FavoriteService.getFavoriteIngredient(myAuthentication, _context);
+        }
+        // 获取用户收藏的菜谱
+        [Route("getFavoriteMenu")]
+        [HttpGet]
+        public async Task<ActionResult<List<Menu>>> getFavoriteMenu([FromHeader] string myAuthentication)
+        {
+            return await FavoriteService.getFavoriteMenu(myAuthentication, _context);
+        }
+    }
+}
---
+++ JiaYaoBackEnd/Services/FavoriteService.cs
@@ -0,0 +1,53 @@
+﻿using JiaYao.Authorization;
+using JiaYao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JiaYao.Services
+{
+    public class FavoriteService
+    {
+        // 获取用户收藏的食材
+        public static async Task<List<Ingredient>> getFavoriteIngredient(string token, JiaYaoContext context)
+        {
+            List<Ingredient> result = new List<Ingredient>();
+            User? user = MemoryCacheHelper.Get(token) as User;
+            if (user != null)
+            {
+                var ingredientIds = context.IngredientFavorites.Where(i => i.UserId == user.Id);
+                result = context.Ingredients.Join(ingredientIds, i => i.Id, ids => ids.IngredientId, (i, ids) => new Ingredient
+                {
+                    Id = i.Id,
+                    Name = i.Name,
+                    Url = i.Url,
+                    Introduction = i.Introduction,
+                    Category = i.Category
+                }).ToList();
+            }
+            return result;
+        }
+        // 获取用户收藏的菜谱
+        public static async Task<List<Menu>> getFavoriteMenu(string token, JiaYaoContext context)
+        {
+            List<Menu> result = new List<Menu>();
+            User? user = MemoryCacheHelper.Get(token) as User;
+            if (user != null)
+            {
+                var menuIds = context.MenuFavorites.Where(i => i.UserId == user.Id);
+                result = context.Menus.Join(menuIds, i => i.Id, ids => ids.MenuId, (i, ids) => new Menu
+                {
+                    Id = i.Id,
+                    Name = i.Name,
+                    Image = i.Image,
+                    Introduction = i.Introduction,
+                    Content = i.Content,
+                    Category = i.Category,
+                    UserId = i.UserId
+                }).ToList();
+            }
+            return result;
+        }
+    }
+}
